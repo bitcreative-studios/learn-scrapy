c8a37ff109b0231cb955fe79cee0e8d45096cdbb
George Bockari <bitwhys@gmail.com>
2020-01-20 20:31:34 -0800

    feat: completes the implementation of cuntries spider

--- worldmeters/worldmeters/spiders/countries.py
+++ worldmeters/worldmeters/spiders/countries.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy.http import Response
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -25,4 +26,20 @@ class CountriesSpider(scrapy.Spider):
             # following links (3 methods)
             # absolute_url = f"https://www.worldometers.info{link}" --> yield scrapy.Response(url=absolute_url)
             # absolute_url = response.urljoin(link) --> yield scrapy.Response(url=absolute_url)
-            yield response.follow(url=link)
+            yield response.follow(
+                url=link, callback=self.parse_country, meta={"country_name": name}
+            )
+
+    def parse_country(self, response):
+        """
+        :param response: The response object
+        :type response: Response
+        """
+        name = response.request.meta["country_name"]
+        rows = response.xpath(
+            "(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr"
+        )
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+            yield {"year": year, "population": population, "country_name": name}
